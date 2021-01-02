7bddf6849db74a481ab0a7dd7ae853f69e9e71fd
omargamal22 <gamalomar808@gmail.com>
2021-01-02 17:55:59 +0200

    Update main.js
    i fixed the RBQ moves and removef Fillaxilibale move from every piece and called fillAvilableMove to all pieces after initializtion

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
-//Link with UI --> each square will have
-//id = 1,1 -> 8,8 and onclick=handleClick(this.id)
+
 function handleClick(position) {
-  //fill += king?
-  //check position in map to get its state
-  /////flag selected
-  //select or deselect
-  console.log(position.id);
+  //console.log(position.id);
   var x = parseInt(position.id.split("-")[0]);
   var y = parseInt(position.id.split("-")[1]);
   var newSelection = helperObj.map[x][y]; //could think of a map(position) as a getter function from map
   if (isSelected) {
     if (newSelection === selected) {
-      //user clicked twice -> Deselect
       Deselect();
     } else if (newSelection == null) {
-      //Empty Square => if available call move else deselect
       if (selected.moves.some((o) => o.x == x && o.y == y)) {
         helperObj.moveToMap_and_ui(selected, x, y);
         moveMap(x, y);
@@ -22,10 +15,8 @@ function handleClick(position) {
         Deselect();
       }
     } else if (newSelection.color == turn) {
-      //clicked a friend piece
-      //change selection to the new piece
       selected = newSelection;
-    } //(newSelection.color != turn) then it is an enemy piece
+    } 
     else {
       if (selected.moves.some((o) => o.x == newSelection.position.x && o.y == newSelection.position.y)) {
         //TAKE ////
@@ -39,7 +30,7 @@ function handleClick(position) {
     if(newSelection)
     if (newSelection.color == turn) {
       selected = newSelection;
-      isSelected = true; //ignore if first click and not your turn;
+      isSelected = true;
     }
   }
   
@@ -59,40 +50,29 @@ function handleClick(position) {
     for (var i = 0; i < selected.moves.length; i++) {
       id = selected.moves[i];
       id = id.x + "-" + id.y; //test
-      console.log("id = " + id);
+      //console.log("id = " + id);
       oldStates.push(id);
       document.getElementById(id).classList.add("highlight"); //the available square
     }
 
   }
-  //////////NEED TO implement the three classed in css
-  //including their hover style
-  //the board could be smaller
-
-  //highlight / dehighlight
-  //game end scenarios to be handled in move!
-  /////////IMPLEMENT Value OF/To string for piece.position to return ready id
 }
-var turn = 0; //white to start
-var selected = null; //no pieces selected initially
+var turn = 0; 
+var selected = null;
 var isSelected = false;
 var oldStates=[];
 
-//To be initialized with piece ids from both sides (e.g. int from 0 to 5) 0 1 2
-//Will only get updated when there is a TAKE action
-//Will be used to check the draw case of insufficient material
-var W = { RemainingArrayOfPieces: [] }; //timer later
+
+var W = { RemainingArrayOfPieces: [] };
 var B = { RemainingArrayOfPieces: [] };
-//---note : reference (one obj to be updated one time)
 
 function moveMap(x, y) {
-  //will move in the backend
   var tX = selected.position.x; tY = selected.position.y;
   var flag = false;
   helperObj.map[tX][tY] = null;
 
   if(selected.firstMove  && Math.abs(tY - y) == 2)
-    selected.firstMove =false;// for handel first move of pawn
+    selected.firstMove =false;
   if(selected.firstMove != undefined &&(y==8 || y==1)){
     selected =  new queen(tX, tY, selected.color);
     flag = true;
@@ -109,29 +89,24 @@ function moveMap(x, y) {
         helperObj.map[i][j].getAndFillAvailableMoves();
     }
   }
-  //CheckCHECK()
-  //CheckGameEnd()
+  
   Deselect();
   turn = !turn;
   setTimeout(function(){
     if(flag)
     alert("Congratulation, Now Pawn become Queen");
   },1000)
-  
-  //Highlight Old and new Moves until any click is made
 }
 function Deselect() {
-  selected = null; //no pieces selected initially
+  selected = null; 
   isSelected = false;
 }
 //-----------------------------------------------------------
 
 //-------------------------------------------
 var helperObj = {
-  //map[x][y]
-  // 0 for white
-  // 1 for black
-  map: [], //to be initialized with piece objs with initial positions
+  
+  map: [], 
   Initialize: function () {
     this.map = [];
     for (var i = 0; i < 9; i++) {
@@ -142,7 +117,12 @@ var helperObj = {
     }
     this.fillInitialize(1, 2, 0);
     this.fillInitialize(8, 7, 1);
-    //this.map[2][7]=null;
+    for (var i = 1; i <= 8; i++) {
+      for (var j = 1; j <= 8; j++) {
+        if (helperObj.map[i][j] != null)
+          helperObj.map[i][j].getAndFillAvailableMoves();
+      }
+    }
   },
 
   fillInitialize: function (_y1, _y2, c) {
@@ -157,8 +137,6 @@ var helperObj = {
     this.map[4][_y1] = new queen(4, _y1, c);
     this.map[5][_y1] = new king(5, _y1, c);
   },
-  //map[1][3] =
-  //map:[[obj1,obj2,null,new new],[],[],[],[],[],[],[]], //to be initialized with piece objs with initial positions
 
   moveToMap_and_ui: function (piece, x, y) {
     let Allpieces = document.querySelectorAll(".black-piece, .white-piece");
@@ -203,17 +181,7 @@ var helperObj = {
             piece.style.transform = `translate(${oldPosition})`;
           }*/
     }
-    //Will only get called in the handleClick method if selection is active (a piece selected in my turn)
-    //Will actually MOVE the piece in the map
-    //Will check target square (in the map) for two cases
-    //-- if empty then just move else TAKE! (move and remove the piece on that square (map & UI))
-    //update the piece's position attribute
-    //Then call all map's pieces fillAvailableMoves() to update them ...... xxxx
-    //Then checkCHECK() after movement! then toggle turn.
-    ///at last moveUI() and the UI
-    /////////// or update on click
-    //------------------------------
-    //-----moveUI();
+    
   },
   //three functions prototypes --implement removeFriendIntersection()
   removeFriendIntersection: function (piece) {
@@ -226,7 +194,7 @@ var helperObj = {
         }
       }
     }
-    console.log(arr);
+    //console.log(arr);
   },
   //filterAvailables:function(){},
   intersection: function (arr1, arr2) {
@@ -249,34 +217,24 @@ var helperObj = {
 helperObj.Initialize();
 
 function Position(_x, _y) {
-  //factory for position
   var p = { x: _x, y: _y };
   return p;
 }
-//could think of it as a class to implement its toString to get the id from position
-//this.getPos = function () { return this.position.x + "-" + this.position.y; }
 function piece(_x, _y, c) {
-  //We will pass the initial position here
   this.position = Position(_x, _y);
   this.color = c;
-  this.moves = []; //array of position objs
+  this.moves = []; 
 
-  this.getAndFillAvailableMoves = function () {}; //abstract
+  this.getAndFillAvailableMoves = function () {};
   this.filterAvailables = function () {
-    helperObj.removeFriendIntersection(this); //to be implemented -- will be called in all pieces
-    //helperObj.checkPinning(this); //Am I pinned?
-    //helperObj.checkCHECK(this); //Is "MY" king in danger?
+    helperObj.removeFriendIntersection(this); 
   };
 }
-//All coming pieces will inherit (those two lines of code) the above structure
-//only the king will not call the filter function inside him because he can't be pinned
-//and apply constructor chaining
+
 function knight(_x, _y, c) {
-  //Constructor chain using call to initialize my vars and the same for the rest
+  
   piece.call(this, _x, _y, c);
-  //two initial positions for white and two for black ...
-  //you can get all pieces available positions from the board
-  //--by counting from 1-8 from the bottom left corner as 1,1
+ 
 
   this.getAndFillAvailableMoves = function () {
     this.moves=[];
@@ -295,73 +253,68 @@ function knight(_x, _y, c) {
     for (var i = 0; i < candidates.length; i++) {
       if (helperObj.InBound(candidates[i])) this.moves.push(candidates[i]);
     }
-    this.filterAvailables(); //this will be called as it is (no overriding here)
+    this.filterAvailables(); 
   };
-
-  //Calling the fill function
-  this.getAndFillAvailableMoves();
 }
 knight.prototype = Object.create(piece.prototype);
 knight.prototype.constructor = knight;
+
 function queen(_x, _y, c) {
   piece.call(this, _x, _y, c);
   this.getAndFillAvailableMoves = function () {
-    //8 directions
-    //(++x)(--x)(++y)(--y)(+x + y)(-x - y)(+x - y)(-x + y) at every step
-
-    //Will get done using "getLineOfSquaresToFirstElement()" method
-    //to be implemented ...
     this.moves=[];
-    this.moves = getLineOfSquaresToFirstElement(this.position, 1, 1);
-    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, 0, 1));
-    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, 0, -1));
-    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, 1, 0));
-    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, -1, 0));
-    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, -1, -1));
-    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, -1, 1));
-    this. moves = this. moves.concat(getLineOfSquaresToFirstElement(this.position, 1, -1));
-
+    this.moves = getLineOfSquaresToFirstElement(this, 1, 1);// /up
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 0, 1));//up
+    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 0, -1));//down
+    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 1, 0));//-->
+    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, 0));//<--
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, -1));// /down
+    this. moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, 1));// \up
+    this. moves = this. moves.concat(getLineOfSquaresToFirstElement(this, 1, -1));// \down
     this.filterAvailables();
   };
-  this.getAndFillAvailableMoves();
 }
 queen.prototype = Object.create(piece.prototype);
 queen.prototype.constructor = queen;
 function rook(_x, _y, c) {
   queen.call(this, _x, _y, c);
   this.getAndFillAvailableMoves = function () {
-    moves = getLineOfSquaresToFirstElement(this.position, 0, 1);
-    moves = moves.concat(getLineOfSquaresToFirstElement(this.position, 0, -1));
-    moves = moves.concat(getLineOfSquaresToFirstElement(this.position, 1, 0));
-    moves = moves.concat(getLineOfSquaresToFirstElement(this.position, -1, 0));
+    this.moves = getLineOfSquaresToFirstElement(this, 0, 1);
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 0, -1));
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 1, 0));
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, 0));
+    console.log("Rook PosS" + ' '+this.color)
+    console.log( this.moves);
   };
-  this.getAndFillAvailableMoves();
 }
 rook.prototype = Object.create(queen.prototype);
 rook.prototype.constructor = rook;
 function bishop(_x, _y, c) {
   queen.call(this, _x, _y, c);
   this.getAndFillAvailableMoves = function () {
-    this.moves = getLineOfSquaresToFirstElement(this.position, 1, 1);
-    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, -1, -1));
-    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, -1, 1));
-    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this.position, 1, -1));
+    this.moves = getLineOfSquaresToFirstElement(this, 1, 1);
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, -1));
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, -1, 1));
+    this.moves = this.moves.concat(getLineOfSquaresToFirstElement(this, 1, -1));
     this.filterAvailables();
   };
-  this.getAndFillAvailableMoves();
 }
 bishop.prototype = Object.create(queen.prototype);
 bishop.prototype.constructor = bishop;
-function getLineOfSquaresToFirstElement(pos, Xdirction, Ydriction) {
-  var Tpos = new Position(pos.x + Xdirction, pos.y + Ydriction);
+
+function getLineOfSquaresToFirstElement(Piece, Xdirction, Ydriction) {
+  var Tpos = Position(Piece.position.x + Xdirction, Piece.position.y + Ydriction);
   var posS = [];
   while (helperObj.InBound(Tpos) && helperObj.map[Tpos.x][Tpos.y] == null) {
-    posS.push(new Position(Tpos.x, Tpos.y));
+    posS.push(Position(Tpos.x, Tpos.y));
     Tpos.x += Xdirction;
     Tpos.y += Ydriction;
   }
-  if (helperObj.InBound(Tpos) && helperObj.map[Tpos.x][Tpos.y].color != turn)
-    posS.push(new Position(Tpos.x, Tpos.y));
+  if (helperObj.InBound(Tpos) && helperObj.map[Tpos.x][Tpos.y].color != Piece.color){
+    posS.push(Position(Tpos.x, Tpos.y));
+    console.log("fuckkkkkkkkkkkk");
+  }
+ 
   return posS;
 }
 
@@ -409,7 +362,6 @@ function king(_x, _y, c) {
       }
     }
   };
-  this.getAndFillAvailableMoves();
 }
 
 king.prototype = Object.create(piece.prototype);
@@ -465,7 +417,6 @@ function pawn(_x, _y, c) {
     //implement promotion in move method ..... (if pawn & y = 8 -> queen) --level 2
     this.filterAvailables();
   };
-  this.getAndFillAvailableMoves();
 }
 pawn.prototype = Object.create(piece.prototype);
 pawn.prototype.constructor = pawn;
